31d37c2ccf216a5c71c8c6f2fe399e9b103c4443
Gabrielly Vitor <93043622+gabriellyvitor@users.noreply.github.com>
2023-08-14 08:18:51 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -6,6 +6,38 @@
 
 # Crie a função que será avaliada no exercício aqui
 
+def is_anagram(str1, str2):
+    # Remove espaços em branco e converte as strings para minúsculas
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
+
+    # Verifica se os comprimentos das strings são iguais
+    if len(str1) != len(str2):
+        return False
+
+    # Cria dicionários para armazenar a contagem de caracteres em cada string
+    char_count1 = {}
+    char_count2 = {}
+
+    # Conta a ocorrência de cada caractere na primeira string
+    for char in str1:
+        char_count1[char] = char_count1.get(char, 0) + 1
+
+    # Conta a ocorrência de cada caractere na segunda string
+    for char in str2:
+        char_count2[char] = char_count2.get(char, 0) + 1
+
+    # Compara os dicionários de contagem
+    return char_count1 == char_count2
+
+# Teste da função
+string1 = "listen"
+string2 = "silent"
+resultado = is_anagram(string1, string2)
+if resultado:
+    print(f"'{string1}' e '{string2}' são anagramas.")
+else:
+    print(f"'{string1}' e '{string2}' não são anagramas.")
 
 
 
